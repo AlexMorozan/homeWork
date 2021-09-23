22328531e9d82227d4a4ccc369bbf974bc09d6a8
AlexandrWork <iiii.love.ukraine@yandex.ru>
2021-09-23 06:27:37 +0300

    сделал тел книгу на классах, но пока такое

--- callContact/js/script.js
+++ callContact/js/script.js
@@ -1,36 +1,155 @@
-const contact = {
+'use strict'; 
+/* 
+const phoneBook = {
+    phoneContact: [],
+
+    setPhoneContact: (cnt) => {
+        phoneBook.phoneContact.push(cnt);
+    },
+    printPhoneContact: () => {
+        if (phoneBook.phoneContact.length != 0) {
+            for (let i = 0; i < phoneBook.phoneContact.length; i++) {
+                console.log(`Контакт ${i+1}: ${phoneBook.phoneContact[i].printConsoleLog()}`);
+            }
+        } else {
+            console.log("Контактов нет!");
+        }
+    }
+};
+ */
+
+/* const contact = {
     name: {},
     lastName: {},
     callNumber: {},
-    setName: ()=> {
-        contact.name = prompt(`Введите имя контакта`);
-        while(contact.name == null || contact.name == '')
-        {
-            contact.name = prompt(`Введите имя контакта`);
+
+    setName: () => {
+        this.name = prompt(`Введите имя контакта`);
+        while (this.name == null || this.name == '') {
+            this.name = prompt(`Введите имя контакта`);
         }
     },
-    setLastName: ()=> {
-        contact.lastName = prompt(`Введите фамилию контакта`);
-        while(contact.lastName == null || contact.lastName == '')
-        {
-            contact.lastName = prompt(`Введите фамилию контакта`);
+    setLastName: () => {
+        this.lastName = prompt(`Введите фамилию контакта`);
+        while (this.lastName == null || this.lastName == '') {
+            this.lastName = prompt(`Введите фамилию контакта`);
         }
     },
-    setNumber: ()=> {
-        contact.callNumber = prompt(`Введите номер телефона контакта`);
-        while(contact.callNumber == null || contact.callNumber == '')
-        {
-            contact.callNumber = prompt(`Введите номер телефона контакта`);
+    setNumber: () => {
+        this.callNumber = prompt(`Введите номер телефона контакта`);
+        while (this.callNumber == null || this.callNumber == '') {
+            this.callNumber = prompt(`Введите номер телефона контакта`);
         }
     },
-    setContact: ()=>{
-        contact.setName();
-        contact.setLastName();
-        contact.setNumber();
+    setContact: () => {
+        this.setName();
+        this.setLastName();
+        this.setNumber();
     },
-    print: ()=>{
-        alert(`Имя: ${contact.name}  Фамилия: ${contact.lastName}  Номер телефона: ${contact.callNumber}`);
+    print: () => {
+        alert(`Имя: ${this.name}  Фамилия: ${this.lastName}  Номер телефона: ${this.callNumber}`);
+    },
+    printConsoleLog: () => {
+        console.log(`Имя: ${this.name}  Фамилия: ${this.lastName}  Номер телефона: ${this.callNumber}`);
     }
-};
-contact.setContact();
-contact.print();
+}; */
+
+/* let a = new contact();
+const b = new contact();
+a.setContact();
+b.setContact();
+
+phoneBook.setPhoneContact(b);
+phoneBook.setPhoneContact(a);
+
+phoneBook.printPhoneContact();
+
+class asad {
+    constructor(a, b) {
+        this.a1 = a;
+       this.b1 = b;
+    } 
+} */
+
+class phoneBook{
+    constructor(){
+        this.phoneContact = [];
+    }
+
+    setPhoneContact(cnt){
+       this.phoneContact.push(cnt);
+    }
+    printPhoneContact(){
+        if (this.phoneContact.length != 0) {
+            for (let i = 0; i < this.phoneContact.length; i++) {
+                console.log(`Контакт ${i+1}: ${this.phoneContact[i].grtContactString()}`);
+            }
+        } else {
+            console.log("Контактов нет!");
+        }
+    }
+}
+
+class contact{
+    constructor(){
+        this.name = "";
+        this.lastName = '';
+        this.callNumber = 0;
+    }
+
+    setName(){
+        this.name = prompt(`Введите имя контакта`);
+        while (this.name == null || this.name == '') {
+            this.name = prompt(`Введите имя контакта`);
+        }
+    }
+    setLastName(){
+        this.lastName = prompt(`Введите фамилию контакта`);
+        while (this.lastName == null || this.lastName == '') {
+            this.lastName = prompt(`Введите фамилию контакта`);
+        }
+    }
+    setNumber(){
+        this.callNumber = prompt(`Введите номер телефона контакта`);
+        while (this.callNumber == null || this.callNumber == '') {
+            this.callNumber = prompt(`Введите номер телефона контакта`);
+        }
+    }
+    setContact(){
+        this.setName();
+        this.setLastName();
+        this.setNumber();
+    }
+    print(){
+        alert(`Имя: ${this.name}  Фамилия: ${this.lastName}  Номер телефона: ${this.callNumber}`);
+    }
+    printConsoleLog(){
+        console.log(`Имя: ${this.name}  Фамилия: ${this.lastName}  Номер телефона: ${this.callNumber}`);
+    }
+
+    grtContactString(){
+        return `Имя: ${this.name}  Фамилия: ${this.lastName}  Номер телефона: ${this.callNumber}`;
+    }
+}
+
+const phBook = new phoneBook();
+const cnt1 = new contact();
+/* const cnt2 = new contact();
+const cnt3 = new contact(); */
+
+cnt1.setContact();
+/* cnt2.setContact();
+cnt3.setContact(); */
+
+phBook.setPhoneContact(cnt1);
+/* phBook.setPhoneContact(cnt2);
+phBook.setPhoneContact(cnt3); */
+let tmp;
+for(let i = 0; i < 5; i++)
+{
+    let temp = new contact();
+    temp.setContact();
+   phBook.setPhoneContact(temp);
+}
+
+phBook.printPhoneContact();
